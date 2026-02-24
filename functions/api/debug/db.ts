ff3e4b4c78df839d75dc59fa55cc98c8c0439aba
Michail <michail@agent-platform.com>
2026-02-24 21:02:23 +0500

    chore: remove debug endpoints

--- functions/api/debug/db.ts
+++
@@ -1,9 +0,0 @@
-export const onRequestGet: PagesFunction<{ DB: D1Database }> = async ({ env }) => {
-  const r = await env.DB.prepare(
-    "SELECT name FROM sqlite_master WHERE type='table' ORDER BY name;"
-  ).all();
-
-  return new Response(JSON.stringify(r.results ?? r, null, 2), {
-    headers: { "content-type": "application/json; charset=utf-8" },
-  });
-};
