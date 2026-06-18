cdb440a68e7da39ff74689e89e59d30a72d48791
Michael-edel <mikhaileirich@gmail.com>
2026-06-18 22:57:39 +0500

    Add PDF export for progress archive

--- pdf/index.ts
+++ pdf/index.ts
@@ -1,3 +1,4 @@
 export { downloadShortHealthReportPdf } from './shortReport';
 export { downloadDetailedHealthReportPdf } from './detailedReport';
 export { downloadProgressComparisonPdf } from './progressComparison';
+export { downloadProgressArchivePdf } from './progressArchive';
