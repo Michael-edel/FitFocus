56c89e034f4d7a3b3cb3f1349b3998f5badf153b
Michael-edel <mikhaileirich@gmail.com>
2026-06-19 17:21:05 +0500

    Fix Pages API routing on preview domains

--- index.tsx
+++ index.tsx
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { initPWA } from './pwa';
 
+const CANONICAL_APP_HOST = 'fitfocus.pages.dev';
+const isPreviewPagesHost =
+  typeof window !== 'undefined' &&
+  window.location.hostname.endsWith('.pages.dev') &&
+  window.location.hostname !== CANONICAL_APP_HOST;
+
+if (isPreviewPagesHost) {
+  const target = new URL(window.location.href);
+  target.protocol = 'https:';
+  target.host = CANONICAL_APP_HOST;
+  window.location.replace(target.toString());
+}
+
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error('Could not find root element to mount to');
