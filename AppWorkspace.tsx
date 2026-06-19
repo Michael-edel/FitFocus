a67daae61c979b58ec16f49c84f1d7f2369efe64
Michael-edel <mikhaileirich@gmail.com>
2026-06-19 10:50:27 +0500

    Persist camera facing preference

--- AppWorkspace.tsx
+++ AppWorkspace.tsx
@@ -28,7 +28,7 @@ export default function AppWorkspace({ workspaceProps }: AppWorkspaceProps) {
 
   const { planTaskDone, setPlanTaskDone, setPlanIntroOpen, setPlanRulesExpanded, planRulesExpanded, planWeekExpanded, setPlanWeekExpanded, weeklyMenuLoading, handleGenerateWeeklyMenu, weeklyMenuError, currentUserAiPlan, currentUserTargetWeight, formatGramsPretty, MealParts, cloudFamily, planScope, setPlanScope, familyShoppingLoading, familyShopping, toggleFamilyShoppingItem, loadFamilyShopping, familyMenuError, familyMenu, familyMenuLoading, setFamilyMenuPrefsOpen, handleGenerateFamilyWeeklyMenu, paywallPlan, allUsers, currentUserGoal = Goal.MAINTAIN, DEFAULT_DEFICIT, DEFAULT_SURPLUS } = plan;
 
-  const { cameraOpen, setCameraOpen, handlePhotoUpload, processPhotoFiles, remainingScans, searchQuery, setSearchQuery, showSearchResults, setShowSearchResults, searchResults, addFoodToDiary, foodDiary, selectedFoodIds, toggleFoodSelected, bulkUpdateMealType, bulkRemoveSelectedFoods, deleteFoodEntry, deleteFoodPhoto, openInsight, openEditFood, formatTime, mealTypeLabel, MacroBarComponent, FoodDiaryGroupedComponent } = nutrition;
+  const { cameraOpen, setCameraOpen, cameraFacing, setCameraFacing, handlePhotoUpload, processPhotoFiles, remainingScans, searchQuery, setSearchQuery, showSearchResults, setShowSearchResults, searchResults, addFoodToDiary, foodDiary, selectedFoodIds, toggleFoodSelected, bulkUpdateMealType, bulkRemoveSelectedFoods, deleteFoodEntry, deleteFoodPhoto, openInsight, openEditFood, formatTime, mealTypeLabel, MacroBarComponent, FoodDiaryGroupedComponent } = nutrition;
 
   const { favoriteRecipes, addFavoriteRecipe, removeFavoriteRecipe, clearFavoriteRecipes } = content;
   const { measurementsHistory, progressPhotos, wearableProvider, wearableEnabled, wearableConnectedAt, wearableLastSyncAt, wearableStepsToday, wearableActiveMinutesToday, wearableSleepHoursLastNight, wearableMetricsUpdatedAt, onPatchUser, syncState } = progress;
@@ -174,6 +174,8 @@ export default function AppWorkspace({ workspaceProps }: AppWorkspaceProps) {
           <NutritionScreen
             cameraOpen={cameraOpen}
             setCameraOpen={setCameraOpen}
+            cameraFacing={cameraFacing}
+            setCameraFacing={setCameraFacing}
             handlePhotoUpload={handlePhotoUpload}
             processPhotoFiles={processPhotoFiles}
             remainingScans={remainingScans}
